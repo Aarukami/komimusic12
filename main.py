42a42ca65369f2510f10d5d226f76382b7c6d011
Aarukami <91074890+Aarukami@users.noreply.github.com>
2022-04-03 16:34:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import asyncio
 from pytgcalls import idle
-from driver.veez import call_py, bot
-
+from driver.veez import call_py, STRING5, STRING4, STRING3, STRING2, STRING1
 async def start_bot():
-    print("[INFO]: STARTING BOT CLIENT")
-    await bot.start()
-    print("[INFO]: STARTING PYTGCALLS CLIENT")
-    await call_py.start()
+ if STRING1 != "None":
+        await pytgcalls1.start()
+    if STRING2 != "None":
+        await pytgcalls2.start()
+    if STRING3 != "None":
+        await pytgcalls3.start()
+    if STRING4 != "None":
+        await pytgcalls4.start()
+    if STRING5 != "None":
+        await pytgcalls5.start()
     await idle()
     print("[INFO]: STOPPING BOT & USERBOT")
     await bot.stop()
