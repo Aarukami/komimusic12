51784727e33c318e40c8d16737e574f2efdc878b
Aarukami <91074890+Aarukami@users.noreply.github.com>
2022-05-10 11:38:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,19 +2,10 @@ import asyncio
 from pytgcalls import idle
 from driver.veez import call_py, STRING5, STRING4, STRING3, STRING2, STRING1
 async def start_bot():
-    if STRING1 != "None":
-        await pytgcalls1.start()
-    if STRING2 != "None":
-        await pytgcalls2.start()
-    if STRING3 != "None":
-        await pytgcalls3.start()
-    if STRING4 != "None":
-        await pytgcalls4.start()
-    if STRING5 != "None":
-        await pytgcalls5.start()
-        await idle()
-    print("[INFO]: STOPPING BOT & USERBOT")
-    await bot.stop()
+    print("[INFO]: STARTING BOT CLIENT")
+    await bot.start()
+    print("[INFO]: STARTING PYTGCALLS CLIENT")
+    await call_py.start()
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(start_bot())
