d07ad0f19c2537c9ad82d2ffe5ca542240c779dc
Aarukami <91074890+Aarukami@users.noreply.github.com>
2022-05-10 11:37:09 +0530

    Update veez.py

--- driver/veez.py
+++ driver/veez.py
@@ -10,32 +10,10 @@ bot = Client(
     plugins={"root": "program"},
 )
 
-if not STRING1:
-    ASS_CLI_1 = None
-else:
-    ASS_CLI_1 = Client(api_id=API_ID, api_hash=API_HASH, session_name=STRING1, plugins=dict(root="AvengerMusic.Plugins.Multi-Assistant"))
+user = Client(
+    SESSION_NAME,
+    api_id=API_ID,
+    api_hash=API_HASH,
+)
 
-
-if not STRING2:
-    ASS_CLI_2 = None
-else:
-    ASS_CLI_2 = Client(api_id=API_ID, api_hash=API_HASH, session_name=STRING2, plugins=dict(root="AvengerMusic.Plugins.Multi-Assistant"))
-
-
-if not STRING3:
-    ASS_CLI_3 = None
-else:
-    ASS_CLI_3 = Client(api_id=API_ID, api_hash=API_HASH, session_name=STRING3, plugins=dict(root="AvengerMusic.Plugins.Multi-Assistant"))
-
-
-if not STRING4:
-    ASS_CLI_4 = None
-else:
-    ASS_CLI_4 = Client(api_id=API_ID, api_hash=API_HASH, session_name=STRING4, plugins=dict(root="AvengerMusic.Plugins.Multi-Assistant"))
-
-
-if not STRING5:
-    ASS_CLI_5 = None
-else:
-    ASS_CLI_5 = Client(api_id=API_ID, api_hash=API_HASH, session_name=STRING5, plugins=dict(root="AvengerMusic.Plugins.Multi-Assistant"))
-
+call_py = PyTgCalls(user, overload_quiet_mode=True)
